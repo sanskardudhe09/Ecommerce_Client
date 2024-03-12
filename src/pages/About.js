9289fa468f6e8ec1eb35b07e992605b80ff0d6f9
sanskardudhe09 <sanskardudhe2018@gmail.com>
2024-03-12 23:23:23 +0530

    modified css

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,6 +5,10 @@ const About = () => {
   return (
     <Layout>
         <div className='row aboutpage justify-content-between'>
+          <div className='responsive'>
+             <img className="aboutimg"src="/images/about.jpg"  alt="about img"/>
+             <h3 class="responsive-head">We at E-Basket provides variety of features including affordable products and efficient order tracking </h3>
+          </div>
           <div className='col-md-6 mt-5'>
              <img className="aboutimg"src="/images/about.jpg"  alt="about img"/>
              <h3 class="responsive-head">We at E-Basket provides variety of features including affordable products and efficient order tracking </h3>
