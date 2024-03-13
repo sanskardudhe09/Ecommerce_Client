675870ecdadfc7dd2edd4178635b148582a00ef1
sanskardudhe09 <sanskardudhe2018@gmail.com>
2024-03-13 15:26:34 +0530

    modified css

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -31,7 +31,7 @@ const Contact = () => {
             </p>
           </div>
           <div className='contactform-responsive'>
-            <h4 className='text-center mt-5 contact-heading' style={{
+            <h4 className='text-center contact-heading' style={{
               letterSpacing: "2px", fontWeight: 'bold'
             }}>With 24x7 support for customers regarding queries and products..</h4>
             <h4 className='text-center mt-3 detail'>Our Details</h4>
